1b06a1d1a8bede564e7261aed43ef44fd1473921
Douglas Porto <douglasalexandre7@gmail.com>
2019-04-17 20:30:43 -0300

    JWT Auth

--- service/src/resolvers/Query.js
+++ service/src/resolvers/Query.js
@@ -1,14 +1,23 @@
-function user (_, args, ctx, info) {
-  return ctx.db.query.user({
-    where: {
-      id: args.id
-    }
-  }, info).then(user => {
-    console.log('User: ', user)
-    return user
-  })
+const { getUserId } = require('./../utils');
+
+function user(_, args, ctx, info) {
+  const userId = getUserId(ctx);
+
+  return ctx.db.query
+    .user(
+      {
+        where: {
+          id: userId
+        }
+      },
+      info
+    )
+    .then(user => {
+      console.log('User: ', user);
+      return user;
+    });
 }
 
 module.exports = {
   user
-}
+};
